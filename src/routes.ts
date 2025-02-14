7cb5acc51524967fac8b663ee37d4b450fc2c48f
m4riott0 <mariottopires@gmail.com>
2025-02-14 11:40:09 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,44 @@
+import { Router, Request, Response } from 'express'
+
+import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { UpdateUserController } from './controllers/user/UpdateUserController';
+
+import { CreateHaircutController } from './controllers/haircut/CreateHaircutController';
+import { ListHaircutController } from './controllers/haircut/ListHaircutController';
+import { UpdateHaircutController } from './controllers/haircut/UpdateHaircutController';
+import { CheckSubscriptionController } from './controllers/haircut/CheckSubscriptionController';
+import { CountHaircutsController } from './controllers/haircut/CountHaircutsController'
+import { DetailHaircutController } from './controllers/haircut/DetailHaircutController';
+
+import { NewScheduleController } from './controllers/schedule/NewScheduleController';
+import { ListScheduleController } from './controllers/schedule/ListScheduleController';
+import { FinishScheduleController } from './controllers/schedule/FinishScheduleController'
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
+
+const router = Router();
+
+//--ROTAS USER--
+router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle) 
+router.get('/me', isAuthenticated, new DetailUserController().handle)
+router.put('/user', isAuthenticated, new UpdateUserController().handle)
+
+//--ROTA HAIRCUT--
+router.post('/haircut', isAuthenticated, new CreateHaircutController().handle)
+router.get('/haircuts', isAuthenticated, new ListHaircutController().handle)
+router.put('/uphaircut', isAuthenticated, new UpdateHaircutController().handle)
+router.get('/check', isAuthenticated, new CheckSubscriptionController().handle)
+router.get('/count', isAuthenticated, new CountHaircutsController().handle)
+router.get('/detail', isAuthenticated, new DetailHaircutController().handle)
+
+// --- ROTA SCHEDULE / SERVIÇOS ---
+router.post('/newschedule', isAuthenticated, new NewScheduleController().handle)
+router.get('/listschedule', isAuthenticated, new ListScheduleController().handle)
+router.delete('/finishschedule', isAuthenticated, new FinishScheduleController().handle)
+
+
+export { router };
